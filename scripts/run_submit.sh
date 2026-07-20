6a432cf2f5e1ffdb51295f7a171208ef8272340d
Tobias Classen <tclassen@wesleyan.edu>
2026-07-20 09:28:41 -0400

    multithreading update

--- scripts/run_submit.sh
+++ scripts/run_submit.sh
@@ -20,6 +20,9 @@ module load gcc/12.2.0
 module load miniforge
 
 # Run your application
+source /home/tclassen/dependencies/geant4-install/bin/geant4.sh
 cd /home/tclassen/projects/build-dspx
 export G4CMP_CRYSTAL_MAPS="/home/tclassen/dependencies/g4cmp-install/share/G4CMP/CrystalMaps"
+sed -i "s|/run/numberOfThreads.*|/run/numberOfThreads ${SLURM_CPUS_PER_TASK:-1}|" ../macros/neutron_gun_batch.macln -sfn /home/tclassen/dependencies/g4cmp-install/share/G4CMP/CrystalMaps/* ./
+mkdir -p ../output/g4sim/q_array_cmp
 ./main ../macros/neutron_gun_batch.mac
